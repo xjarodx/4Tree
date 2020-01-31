05fc5bd5788d4c188acefa28f9d59244b603fa8b
xjarodx <11578521+xjarodx@users.noreply.github.com>
2020-01-31 09:15:04 +0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,41 +1,40 @@
 window.onscroll = function() {
-    stickyNav();
-    logoChange();
-  }
+  stickyNav();
+  logoChange();
+}
 
-  // Get the navbar
-  var navbar = document.getElementById("navbar");
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-  var menuIco = document.getElementById("menIco");
-  
-  // Get the offset position of the navbar
-  var sticky = navbar.offsetTop;
- 
-  function stickyNav() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky");
-    } else {
-      navbar.classList.remove("sticky");
-      }
-    } 
+var menuIco = document.getElementById("menIco");
 
-  function logoChange(){  
-    if (window.pageYOffset >= 300 && menuIco.classList.contains("lines")) {
-      menuIco.classList.replace("lines", "lines2");
-    } else if (window.pageYOffset <= 300 && menuIco.classList.contains("lines2")) {
-      menuIco.classList.replace("lines2", "lines");
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
     }
+  } 
+
+function logoChange(){  
+  if (window.pageYOffset >= 300 && menuIco.classList.contains("lines")) {
+    menuIco.classList.replace("lines", "lines2");
+  } else if (window.pageYOffset <= 300 && menuIco.classList.contains("lines2")) {
+    menuIco.classList.replace("lines2", "lines");
   }
-    
-  function openSlideMenu(){
-      document.getElementById('side-menu').style.width = '15%';
-      //Moves the Main element left when slide menu is opened
-  }
+}
   
-  function closeSlideMenu(){
-      document.getElementById('side-menu').style.width = '0px';
-      //Moves the Main element right when slide menu is closed
-  }
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '15%';
+    //Moves the Main element left when slide menu is opened
+}
 
-  document.body.addEventListener('click', closeSlideMenu, true); 
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0px';
+    //Moves the Main element right when slide menu is closed
+}
 
+document.body.addEventListener('click', closeSlideMenu, true); 
